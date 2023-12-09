d8ee1e18a12977a2c45808c0c1fcd9ca42ff0f17
Alan Opresnik <alanopk328@gmail.com>
2023-12-09 14:20:00 +0100

    upload and delete files banners

--- models/Banner.js
+++ models/Banner.js
@@ -1,10 +1,16 @@
 const { Schema, model } = require("mongoose");
 
 const BannerSchema = Schema({
-    img:{
-        type: String,
-        required: true,
-    }
+    images: [{
+        fieldname: String,
+        originalname: String,
+        encoding: String,
+        mimetype: String,
+        destination: String,
+        filename: String,
+        path: String,
+        size: Number,
+      }],
 })
 
 module.exports = model("Banner", BannerSchema);
