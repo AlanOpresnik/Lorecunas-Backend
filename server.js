0a8b5733d0b5f046b2138ed103ff77d238f9d794
Alan Opresnik <alanopk328@gmail.com>
2023-12-09 15:05:59 +0100

    Revert "route get folder"
    This reverts commit 4f53bbc2fefd584cd3150c5a329a69862fbb914a.

--- server.js
+++ server.js
@@ -34,8 +34,6 @@ app.use("/api/banners", BANNER_ROUTES);
 app.use("/api/oferts", OFERT_ROUTES);
 
 app.use("/uploadsProducts", express.static(path.join(__dirname, "uploadsProducts")));
-app.use("/uploadsBanners", express.static(path.join(__dirname, "uploadsBanners")));
-
 
 
 app.listen(port, () => {
